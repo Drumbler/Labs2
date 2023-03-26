c39a9116accb2852d9fc0901fcb13ef0f0a897cb
Drumbler <andy132012@gmail.com>
2023-03-26 20:23:56 +0500

    Labs2

---
+++ src/lab1/ex1.java
@@ -0,0 +1,42 @@
+package lab1;
+
+import java.util.InputMismatchException;
+//import java.util.Random;
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int amount = 0;
+        int sum = 0;
+        int size = 10;
+        int[] numbers = new int[size];
+//        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+//        IntStream.range(0,size).forEach(index -> numbers[index] = random.nextInt(-20, 20));
+//        System.out.println(Arrays.toString(numbers));
+
+        System.out.println("Введите числа массива, " + size + " чисел");
+        try {
+            for (int i = 0; i < size; i++) {
+                numbers[i] = scan.nextInt();
+            }
+
+            for (int i = 0; i < size; i++) {
+                if (numbers[i] > 0) {
+                    sum = sum + numbers[i];
+                    amount++;
+                }
+            }
+
+            sum = sum / amount;
+            System.out.println(sum);
+        }
+        catch(ArithmeticException exception) {
+            System.out.println("Ошибка! В массиве только числа с минусом!");
+        }
+        catch (InputMismatchException exception ){
+            System.out.println("Ошибка! Тип данных не соответсвует массиву!");
+        }
+    }
+
+}
